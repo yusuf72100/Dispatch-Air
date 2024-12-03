030d09338710caad0dd3df32c457828d569aa3f1
Sultan <38288876+yusuf72100@users.noreply.github.com>
2024-12-04 00:01:43 +0100

    First Window Responsive

--- src/main/java/org/Affichage/MainMenu.java
+++ src/main/java/org/Affichage/MainMenu.java
@@ -1,5 +1,6 @@
 package org.Affichage;
 
+import javafx.animation.*;
 import javafx.application.Platform;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
@@ -7,13 +8,12 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Priority;
-import javafx.scene.layout.Region;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 import org.launcher.Launcher;
 
 import static org.launcher.Launcher.normaliserChemin;
